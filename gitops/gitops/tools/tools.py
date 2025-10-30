dde2f75d38771d8b9d23a6c1b4cb05e8c942fa64
Karan Adep <karankumar.adep@dexcom.com>
2025-10-30 11:46:47 -0700

    chore: pushing updates

--- gitops/gitops/tools/tools.py
+++ gitops/gitops/tools/tools.py
@@ -9,52 +9,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-argocd_tools = MCPToolset(
-    connection_params=StreamableHTTPConnectionParams(
-        url="http://localhost:3000/mcp/",
-        headers={
-            "x-argocd-base-url": os.getenv("ARGOCD_BASE_URL"),
-            "x-argocd-api-token": os.getenv("ARGOCD_API_TOKEN"),
-        },
-    ),
-    # Read only tools
-    tool_filter=[
-        "list_applications",
-        "get_application",
-        "create_application",
-        "update_application",
-        "delete_application",
-        "sync_application",
-        "get_application_resource_tree",
-        "get_application_managed_resources",
-        "get_application_workload_logs",
-        "get_resource_events",
-        "get_resource_actions",
-        "run_resource_action",
-    ],
-)
-
 # argocd_tools = MCPToolset(
-#     # errlog=None,
-#     connection_params=StdioConnectionParams(
-#         server_params = StdioServerParameters(
-#             command='npx',
-#             args=[
-#                 "-y",
-#                 "argocd-mcp@latest",
-#                 "stdio"
-#             ],
-#             # Pass the API key as an environment variable to the npx process
-#             # This is how the MCP server for Google Maps expects the key.
-#             env={
-#                 "ARGOCD_BASE_URL": os.getenv("ARGOCD_BASE_URL"),
-#                 "ARGOCD_API_TOKEN": os.getenv("ARGOCD_API_TOKEN"),
-#                 # Add this if you are using self-signed certificates
-#                 # "NODE_TLS_REJECT_UNAUTHORIZED": "0",                
-#             }
-#         ),
+#     connection_params=StreamableHTTPConnectionParams(
+#         url="http://localhost:3000/mcp/",
+#         headers={
+#             "x-argocd-base-url": os.getenv("ARGOCD_BASE_URL"),
+#             "x-argocd-api-token": os.getenv("ARGOCD_API_TOKEN"),
+#         },
 #     ),
-#     # You can filter for specific Maps tools if needed:
+#     # Read only tools
 #     tool_filter=[
 #         "list_applications",
 #         "get_application",
@@ -70,6 +33,43 @@ argocd_tools = MCPToolset(
 #         "run_resource_action",
 #     ],
 # )
+
+argocd_tools = MCPToolset(
+    # errlog=None,
+    connection_params=StdioConnectionParams(
+        server_params = StdioServerParameters(
+            command='npx',
+            args=[
+                "-y",
+                "argocd-mcp@latest",
+                "stdio"
+            ],
+            # Pass the API key as an environment variable to the npx process
+            # This is how the MCP server for Google Maps expects the key.
+            env={
+                "ARGOCD_BASE_URL": os.getenv("ARGOCD_BASE_URL"),
+                "ARGOCD_API_TOKEN": os.getenv("ARGOCD_API_TOKEN"),
+                # Add this if you are using self-signed certificates
+                # "NODE_TLS_REJECT_UNAUTHORIZED": "0",                
+            }
+        ),
+    ),
+    # You can filter for specific Maps tools if needed:
+    tool_filter=[
+        "list_applications",
+        "get_application",
+        "create_application",
+        "update_application",
+        "delete_application",
+        "sync_application",
+        "get_application_resource_tree",
+        "get_application_managed_resources",
+        "get_application_workload_logs",
+        "get_resource_events",
+        "get_resource_actions",
+        "run_resource_action",
+    ],
+)
 
 # github_tools = MCPToolset(
 #     errlog=None,    
